3fc6da5ebc4ae12a26ce744bee126bb22c661f4d
Sampurna <sampurng@gmail.com>
2024-09-26 12:15:51 -0400

    Server API for healthz

---
+++ src/connect.ts
@@ -0,0 +1,17 @@
+export const connectToDb = async (res: any) => {
+  const { Sequelize } = require("sequelize");
+
+  const connection = new Sequelize(process.env.DB_CONNECTION_URL);
+
+  try {
+    await connection.authenticate();
+
+    console.log("Connection has been established successfully.");
+
+    return 200;
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+
+    return 503;
+  }
+};
