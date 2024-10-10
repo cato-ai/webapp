6a1f999be0352abb1ef56b303b96b4bc49ee0c96
Sampurna <sampurng@gmail.com>
2024-10-10 14:31:49 -0400

    Updated test command and fixed all tests

--- src/startup.ts
+++ src/startup.ts
@@ -2,7 +2,7 @@ import { sequelize } from "./connect";
 
 export const startup = async () => {
   try {
-    await sequelize.sync({ force: false, logging: false }); // Set `force: true` to drop and recreate tables
+    await sequelize.sync({ force: true, logging: false }); // Set `force: true` to drop and recreate tables
     console.log("Database synced successfully");
   } catch (error) {
     console.log("Cannot startup DB", error);
